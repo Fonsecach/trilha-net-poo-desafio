577355b545c5129f31fb76c435a28a123dd8498e
Cleiton H. da Fonseca <chdf@proton.me>
2023-12-24 16:35:44 -0300

    Primeira atualização
    Implementação parcial classe nokia

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,34 @@
 namespace DesafioPOO.Models
 {
+
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
+        public Nokia(){
+            
+        }
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            string confirmacao = "";
+            do {
+                Console.WriteLine("Insira o nome do aplicativo: ");
+                nomeApp = Console.ReadLine();
+                Console.WriteLine("Confirma a instalação? (Y/N): ");
+                confirmacao = Console.ReadLine().ToUpper();
+
+                if(confirmacao == "Y"){
+                    Console.WriteLine("-= Instalando o aplicativo =-");
+                    Console.WriteLine("-=-=-=-");
+                    Console.WriteLine($"Aplicativo instalado com sucesso!");
+                } else {
+                    Console.WriteLine("Operação cancelada!");
+                }
+
+                confirmacao = "N";
+                Console.WriteLine("Deseja inserir outro aplicativo? (Y/N)");
+                confirmacao = Console.ReadLine().ToUpper();
+            } while (confirmacao == "Y");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
+public class Program {
+    public static void Main(string[] args){
+
+
+    }
+}
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
